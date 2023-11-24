56a90a94cddc32b985b2faf7a71468d773f872f2
Harok <41049683+dorianharok@users.noreply.github.com>
2023-11-24 16:44:09 +0900

    string 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,43 @@
 package study;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split(){
+        String[] arr = "1,2".split(",");
+        assertThat(arr).contains("1", "2");
+    }
+
+    @Test
+    void split2(){
+        String[] arr = "1".split(",");
+        assertThat(arr).containsExactly("1");
+    }
+
+    @Test
+    void substring(){
+        String str = "(1,2)";
+        String actual = str.substring(1, str.length() - 1);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt Exception 테스트")
+    void exception(){
+        String str = "abc";
+        assertThatThrownBy(() ->
+            str.charAt(3)
+        ).isInstanceOf(IndexOutOfBoundsException.class)
+            .hasMessageContaining("String index out of range: 3");
     }
 }
